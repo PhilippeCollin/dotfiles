30bb1dbd01ada504cd423e42992232aedbd77f8e
Philippe Collin <philippe.collin@shopify.com>
2021-11-12 15:59:26 -0500

    temporary aliases for isospin

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -6,4 +6,21 @@ fi
 
 if [ $SPIN ]; then
     alias rnginx="sudo pkill -9 nginx && sudo service nginx start"
+
+
+    function logf() {
+        journalctl -u proc-shopify--"$1"@server.service -f
+    }
+
+    function restart() {
+        systemctl restart proc-shopify--"$1"@server.service
+    }
+
+    function start() {
+        systemctl start proc-shopify--"$1"@server.service
+    }
+
+    function stop() {
+        systemctl stop proc-shopify--"$1"@server.service
+    }
 fi
